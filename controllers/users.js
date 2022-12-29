073d931c38e22e7621c9fb8e2cf644d258acc3fa
Abhimanyu Kumar <abhimanyukumar82982@gmail.com>
2022-12-29 19:59:07 +0530

    additional features added!

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,45 @@
 
-
-const users = [];
+import { v4 as uuidv4 } from 'uuid';
+let users = [];
 
 export const getUsers = (req, res) => {
     res.send(users);
+}
+
+export const getUser = (req, res) => {
+    const user = users.find(user => user.id === req.params.id);
+    if(user == null){
+        res.send('No user find with given id!')
+    }else{
+        res.send(user);
+    }
+}
+
+export const addUser = (req, res) => {
+    const user = req.body;
+    users.push({...user, id: uuidv4()});
+    res.send(`${user.firstName} added successfully!`)
+}
+
+export const updateUser = (req, res) => {
+    const user = users.find(user => user.id === req.params.id);
+    if(user == null){
+        res.send('No user find with given id!')
+    }else{
+        user.firstName = req.body.firstName;
+        user.lastName = req.body.lastName;
+        user.age = req.body.age;
+        user.mobNumber = req.body.mobNumber;
+        res.send(`User with id: ${req.params.id} is updated!`);
+    }
+}
+
+export const deleteUser = (req, res) => {
+    const user = users.find(user => user.id === req.params.id);
+    if(user == null){
+        res.send('No user find with given id!')
+    }else{
+        users = users.filter(obj => obj.id !== req.params.id);
+        res.send(`User with id: ${req.params.id} is deleted!`);
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 import express from 'express';
 const router = express.Router();
-import { getUsers } from '../controllers/users.js';
+import { getUsers, getUser, addUser, updateUser, deleteUser } from '../controllers/users.js';
 
 
 router.get('/', getUsers);
+router.get('/:id', getUser);
+
+router.post('/', addUser);
+
+router.patch('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
 
 
 export default router;
